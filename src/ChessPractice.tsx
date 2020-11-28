59c40ec0caf819f1b50cf15ae568ad44bafa4cd5
Warren Falk <warren@warrenfalk.com>
2020-11-28 10:56:21 -0500

    basic react app

---
+++ src/ChessPractice.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ChessPractice: React.FC = ({}) => {
+    return (
+        <div>Chess Practice</div>
+    )
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Got here");
+import { createElement } from 'react';
+import { render } from 'react-dom';
+import { ChessPractice } from './ChessPractice';
+
+(async function () {
+    const app = createElement(ChessPractice);
+    render(app, document.getElementById('root'));
+})();
+
